b5a3eedf7e92928f97275f4c765e9b4703b7dccd
tomas9807 <tomas9807@protonmail.com>
2018-07-19 11:39:09 -0400

    0.1

---
+++ myapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    # path('', views.home, name = 'home'),
+    # path('login/',views.login,name='login'),
+    # path('create_acc/',views.create_acc,name='create_acc'),
+    # path('my_acc/',views.my_acc,name='my_acc'),
+    # path('houses/',views.houses,name='houses'),
+    # path('houses/detail/<int:pk>',views.house_detail,name='house_detail'),
+]
